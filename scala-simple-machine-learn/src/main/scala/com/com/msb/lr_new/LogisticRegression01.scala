26e1980bd3d58c9dc2bf2027fbb1413765474b50
shulanztf <1426763507@qq.com>
2020-02-22 19:47:30 +0800

    资料整理，课程资料

---
+++ scala-simple-machine-learn/src/main/scala/com/com/msb/lr_new/LogisticRegression01.scala
@@ -0,0 +1,11 @@
+package com.com.msb.lr_new
+
+/**
+  * @ClassName: LogisticRegression01
+  * @Author: zhaotf
+  * @Description:
+  * @Date: 2020/2/19 0019 
+  */
+object LogisticRegression01 {
+
+}
